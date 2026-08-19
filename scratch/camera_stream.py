852a0944a102bfa6e862c0e32cfae56113cddb85
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2026-08-19 14:04:46 +0200

    Add a small script to stream thermal camera

---
+++ scratch/camera_stream.py
@@ -0,0 +1,28 @@
+import cv2
+
+# Open the default webcam (usually 0)
+cap = cv2.VideoCapture(16, cv2.CAP_V4L2)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # If frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Webcam Stream', frame)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the capture and close windows
+cap.release()
+cv2.destroyAllWindows()
